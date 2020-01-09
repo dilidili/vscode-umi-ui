9ed5eaaa75a6f4086aa4cc709040c8b927d765a1
Wensheng Xu <xws@superid.cn>
2020-01-09 10:39:44 +0800

    [feat] select umi project

---
+++ src/UmiUI.ts
@@ -0,0 +1,92 @@
+import { Terminal, ExtensionContext, window, workspace, commands, WorkspaceFolder } from 'vscode';
+// import { readFile } from './util/fs';
+const { readFileSync } = require('fs');
+const path = require('path');
+
+export default class UmiUI {
+  private _terminal: Terminal | undefined;
+
+  constructor(
+    private _context: ExtensionContext,
+  ) {}
+
+  async validateUmiProject(projects: WorkspaceFolder[], depressError = true) {
+    if (projects && projects.length > 0) {
+      for(let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+
+        try {
+          const pkg = readFileSync(await project.uri.with({ path: path.join(project.uri.fsPath, 'package.json' ) }).fsPath);
+          const pkgJSON = JSON.parse(pkg);
+          const containUmi = Object.keys({...(pkgJSON.dependencies || {}), ...(pkgJSON.devDependencies || {})}).indexOf('umi') >= 0;
+
+          if (containUmi) {
+            return Promise.resolve(project);
+          }
+        } catch(err) {
+          if (!depressError) {
+            window.showErrorMessage('Please select a valid Umi project');
+          }
+          console.error(err);
+        }
+      }
+    }
+
+    return Promise.resolve(null);
+  }
+
+  async getUmiProject(): Promise<WorkspaceFolder | null | undefined> {
+    let validProject = await this.validateUmiProject(workspace.workspaceFolders || []);
+
+    if (!validProject) {
+      // select umi project
+      const selectedFolders = await window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: 'Select Umi Project'
+      });
+      if (selectedFolders && selectedFolders.length > 0) {
+        const projectUri = selectedFolders[0];
+
+        if (projectUri) {
+          return new Promise((c, e) => {
+            workspace.updateWorkspaceFolders(workspace.workspaceFolders ? workspace.workspaceFolders.length : 0, null, { uri: projectUri });
+            const listenWorkspaceChange = workspace.onDidChangeWorkspaceFolders(async (e) => {
+              if (e.added && e.added.length) {
+                listenWorkspaceChange.dispose();
+
+                validProject = await this.validateUmiProject(e.added as WorkspaceFolder[], false); 
+                if (!validProject) {
+                  window.showErrorMessage('Please select a valid Umi project');
+                  c(null);
+                } else {
+                  c(validProject);
+                }
+              }
+            });
+          });
+        } else {
+          return Promise.resolve(null);
+        }
+      }
+    } else {
+      return Promise.resolve(validProject);
+    }
+  }
+
+
+  async start() {
+    const { _terminal, _context } = this;
+
+    const umiWorkspaceFolder = await this.getUmiProject();
+    if (umiWorkspaceFolder) {
+      console.log(umiWorkspaceFolder);
+    }
+
+    // if (!_terminal) {
+    //   this._terminal = window.createTerminal();
+    //   this._terminal.show();
+    // }
+  }
+};
---
+++ src/extension.ts
@@ -0,0 +1,15 @@
+import * as vscode from 'vscode';
+import UmiUI from './UmiUI';
+
+export function activate(context: vscode.ExtensionContext) {
+	const umiUI = new UmiUI(context);
+
+	let disposable = vscode.commands.registerCommand('extension.umiUI', () => {
+		umiUI.start();
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
